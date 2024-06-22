98d73f0d0968e8db67bbb660d230f41765d3dd32
KrishivMahajan <75154573+KrishivMahajan@users.noreply.github.com>
2024-06-22 17:05:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,17 @@ with tb2:
 - Upload the image 
 - The model will predict the disease
  """)
+	dic={
+
+"Fungal Nail Infections (Onychomycosis)": "Fungal nail infections are common and can affect one or more nails. They often appear as discolored, thickened, and brittle nails. Fungal infections are typically caused by dermatophytes, yeast, or molds. Treatment may include topical or oral antifungal medications."
+"Psoriasis": "Psoriasis is a chronic skin condition that can also affect the nails. Nail psoriasis can cause pitting, discoloration, and separation of the nail from the nail bed. It is often associated with skin psoriasis and can be treated with topical steroids or other psoriasis medications."
+"Eczema (Dermatitis)": "Eczema can affect the skin around the nails, leading to redness, swelling, and peeling of the skin. This condition may be associated with itching and discomfort. Managing eczema with moisturizers and topical corticosteroids can help alleviate symptoms."
+"Ingrown Nails (Onychocryptosis)": "Ingrown nails occur when the edges of the nails grow into the surrounding skin, causing pain, redness, and inflammation. Proper nail trimming and avoiding tight-fitting footwear can help prevent ingrown nails. In severe cases, minor surgical procedures may be required."
+"Beau's Lines": "Beau's lines are transverse depressions or ridges that run horizontally across the nails. They can be caused by a variety of factors, including illness, trauma, or nutritional deficiencies. The appearance of Beau's lines can offer insight into the timing of an underlying health issue."
+"Yellow Nail Syndrome": "Yellow nail syndrome is a rare condition that can cause nails to thicken, yellow, and grow more slowly. It may be associated with lymphedema and respiratory issues. Identifying and managing the underlying causes of the syndrome is crucial."
+"Clubbing": "Nail clubbing is characterized by an enlargement of the fingertips and a downward curving of the nails. It can be associated with various lung and heart conditions. If clubbing is observed, it is important to consult a healthcare professional to identify and address the underlying issue."
+"Koilonychia": "Koilonychia is a condition where the nails become concave or spoon-shaped. It can be a sign of iron-deficiency anemia or hemochromatosis, among other conditions. Treating the underlying cause is essential."
+}
 	st.header("TEST YOURSELF")
 	img=st.file_uploader("UPLOAD THE IMAGE OF YOUR NAIL")
 	if img is not None:
@@ -69,6 +80,7 @@ with tb2:
 		with col[1]:
 			st.subheader('Confidence')
 			st.write(con.confidence)
+		st.write(dic.get(name))
 
 with tb3:
 	st.header("ABOUT THE CREATOR")
